9ad5c52518808b254435eb3405232208a48918b4
SebastianCruzMendez <sebastianscm1999cruzzz@gmail.com>
2026-07-06 21:01:37 -0500

    trabajo

---
+++ pokemon/explorar.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+
+async function obtenerPokemon() {
+  const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur");
+  console.log(respuesta);
+
+  const datos = await respuesta.json();
+  console.log(datos.name, datos.id, datos.weight, datos.types, datos.stats, datos.abilities);
+
+  for(let i=0; i<datos.types.length; i++){
+    console.log(datos.types[i].type.name);
+  }
+
+  for(let i=0; i<datos.stats.length; i++){
+    console.log(datos.stats[i].stat.name);
+    console.log(datos.stats[i].base_stat);
+    }
+
+for(let i=0; i<datos.abilities.length; i++){
+    console.log(datos.abilities[i].ability.name);
+}
+
+}
+obtenerPokemon();
